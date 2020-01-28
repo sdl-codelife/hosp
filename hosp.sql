2b30560b372b048d7d552e08bfebfe9d00aefc78
sdl <sdllove1314@qq.com>
2020-01-28 11:25:21 +0800

    sql

--- hosp.sql
+++ hosp.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 21/01/2020 14:53:02
+ Date: 28/01/2020 11:24:50
 */
 
 SET NAMES utf8mb4;
@@ -26,7 +26,7 @@ CREATE TABLE `t_permission`  (
   `role` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `permission` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_permission
@@ -45,7 +45,7 @@ CREATE TABLE `t_role`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `t_role_ibfk_1`(`userid`) USING BTREE,
   CONSTRAINT `t_role_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_role
@@ -62,14 +62,40 @@ CREATE TABLE `t_user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
   `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 258178 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 258179 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (100, 'user', 'user');
+INSERT INTO `t_user` VALUES (100, 'sdltest', 'user');
 INSERT INTO `t_user` VALUES (10000, 'admin', 'admin');
 INSERT INTO `t_user` VALUES (258178, 'sdl', 'sdllove1314');
+
+-- ----------------------------
+-- Table structure for t_userinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `t_userinfo`;
+CREATE TABLE `t_userinfo`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `realname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `sex` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `avator` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `userid` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `userid`(`userid`) USING BTREE,
+  CONSTRAINT `t_userinfo_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_userinfo
+-- ----------------------------
+INSERT INTO `t_userinfo` VALUES (1, 'sdl', NULL, '', NULL, 'sdllove1314@qq.coom', '随风奔跑自由是方向', 100);
+INSERT INTO `t_userinfo` VALUES (2, '小龙', '男', '/static/upload/images/1eb10edb-734b-40ee-8906-3ee09231ccee.jpg', '17344090162', 'sdllove1314@qq.com', 'yigiaogiao', 10000);
+INSERT INTO `t_userinfo` VALUES (3, NULL, NULL, NULL, NULL, NULL, 'sdlaaaaa ', 258178);
 
 SET FOREIGN_KEY_CHECKS = 1;
